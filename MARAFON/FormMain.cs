face36959a8b83b913d85f7c44cb8235321d1019
August <bobmarleyareback@ya.ru>
2021-03-10 18:27:16 +0300

    Регистрация Бегуна (не закончено)/Выбор регистрации бегуна (закончено)

--- MARAFON/FormMain.cs
+++ MARAFON/FormMain.cs
@@ -16,22 +16,12 @@ namespace MARAFON
         public FormMain()
         {
             InitializeComponent();
-            //foreach (User role in Program.dateBases.User)
-            //{
-            //    MessageBox.Show(role.Email);
-            //}
-            InitializeComponent();
-            timer1.Tick += timerEvent_Tick;
-            timer1.Interval = 1000;
-            timer1.Enabled = true;
-            timer1.Start();
         }
-        Timer timer1 = new Timer();
 
         private void timerEvent_Tick(object sender, EventArgs e)
         {
             TimeSpan TimeRemaining = voteTime - DateTime.Now;
-            labelEventTime.Text = TimeRemaining.Days + " дней " + TimeRemaining.Hours + " часов " + TimeRemaining.Minutes + " минут " + TimeRemaining.Seconds + " минут.";
+            labelEventTime.Text = TimeRemaining.Days + " дней " + TimeRemaining.Hours + " часов " + TimeRemaining.Minutes + " минут " + TimeRemaining.Seconds + " секунд.";
         }
 
         private void buttonSponsore_Click(object sender, EventArgs e)
@@ -44,6 +34,13 @@ namespace MARAFON
         private void FormMain_FormClosed(object sender, FormClosedEventArgs e)
         {
             Application.Exit();
+        }
+
+        private void buttonRunner_Click(object sender, EventArgs e)
+        {
+            FormRegisterAsARunner formRegisterAsARunner = new FormRegisterAsARunner();
+            this.Hide();
+            formRegisterAsARunner.Show();
         }
     }
 }
