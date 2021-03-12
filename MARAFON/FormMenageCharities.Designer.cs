24fef18c8423538e7c197b471b5d8779d79abb0c
Павел Токарев <cstud111079@vyatsu.ru>
2021-03-12 20:05:28 +0300

    new form menager charies by Pavel

---
+++ MARAFON/FormMenageCharities.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace MARAFON
+{
+    partial class FormMenageCharities
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelMARATHON = new System.Windows.Forms.Label();
+            this.labelText = new System.Windows.Forms.Label();
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.listViewCompany = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelMARATHON
+            // 
+            this.labelMARATHON.AutoSize = true;
+            this.labelMARATHON.Location = new System.Drawing.Point(93, 17);
+            this.labelMARATHON.Name = "labelMARATHON";
+            this.labelMARATHON.Size = new System.Drawing.Size(135, 13);
+            this.labelMARATHON.TabIndex = 0;
+            this.labelMARATHON.Text = "MARATHON SKILLS 2016";
+            // 
+            // labelText
+            // 
+            this.labelText.AutoSize = true;
+            this.labelText.Location = new System.Drawing.Point(189, 45);
+            this.labelText.Name = "labelText";
+            this.labelText.Size = new System.Drawing.Size(263, 13);
+            this.labelText.TabIndex = 1;
+            this.labelText.Text = "Управление благотворительными организациями";
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.Location = new System.Drawing.Point(12, 12);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(75, 23);
+            this.buttonBack.TabIndex = 2;
+            this.buttonBack.Text = "Назад";
+            this.buttonBack.UseVisualStyleBackColor = true;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(12, 72);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(126, 29);
+            this.buttonAdd.TabIndex = 3;
+            this.buttonAdd.Text = "+ Добавить нового";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            // 
+            // listViewCompany
+            // 
+            this.listViewCompany.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listViewCompany.FullRowSelect = true;
+            this.listViewCompany.GridLines = true;
+            this.listViewCompany.HideSelection = false;
+            this.listViewCompany.Location = new System.Drawing.Point(12, 107);
+            this.listViewCompany.Name = "listViewCompany";
+            this.listViewCompany.Size = new System.Drawing.Size(681, 420);
+            this.listViewCompany.TabIndex = 4;
+            this.listViewCompany.UseCompatibleStateImageBehavior = false;
+            this.listViewCompany.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Лого";
+            this.columnHeader1.Width = 149;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Наименование";
+            this.columnHeader2.Width = 250;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Описание";
+            this.columnHeader3.Width = 204;
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Location = new System.Drawing.Point(598, 72);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(95, 29);
+            this.buttonEdit.TabIndex = 5;
+            this.buttonEdit.Text = "Изменить";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            this.buttonEdit.Click += new System.EventHandler(this.buttonEdit_Click);
+            // 
+            // FormMenageCharities
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 532);
+            this.Controls.Add(this.buttonEdit);
+            this.Controls.Add(this.listViewCompany);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.buttonBack);
+            this.Controls.Add(this.labelText);
+            this.Controls.Add(this.labelMARATHON);
+            this.Name = "FormMenageCharities";
+            this.Text = "Marathon Skills 2016 – Manage charities";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelMARATHON;
+        private System.Windows.Forms.Label labelText;
+        private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.ListView listViewCompany;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Button buttonEdit;
+    }
+}
---
+++ MARAFON/FormMenageCharities.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace MARAFON
+{
+    public partial class FormMenageCharities : Form
+    {
+        MySqlDataAdapter da;
+        DataTable dt;
+        public FormMenageCharities()
+        {
+            InitializeComponent();
+            loadData();
+        }
+        protected void loadData()
+        {
+            listViewCompany.Items.Clear();
+            ImageList image = new ImageList();
+            image.ImageSize = new Size(80, 40);
+
+
+            Bitmap emptyImage = new Bitmap(30, 30);
+
+            using (Graphics gr = Graphics.FromImage(emptyImage))
+            {
+                gr.Clear(Color.White);
+            }
+
+            string sql = "SELECT Charity.CharityName, Charity.CharityDescription, Charity.CharityLogo FROM Charity";
+            MySqlCommand command = new MySqlCommand(sql, Program.connection);
+            da = new MySqlDataAdapter();
+            da.SelectCommand = command;
+            dt = new DataTable();
+            da.Fill(dt);
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                image.Images.Add(new Bitmap(String.Format("image/{0}", dt.Rows[i]["CharityLogo"].ToString())));
+
+                ListViewItem listViewItem = new ListViewItem(new string[] { "", dt.Rows[i]["CharityName"].ToString(), dt.Rows[i]["CharityDescription"].ToString() });
+                listViewItem.ImageIndex = i;
+                listViewCompany.Items.Add(listViewItem);
+            }
+            image.Images.Add(emptyImage);
+            listViewCompany.SmallImageList = image;
+            listViewCompany.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+        }
+
+        private void buttonEdit_Click(object sender, EventArgs e)
+        {
+            if (listViewCompany.SelectedItems.Count == 1)
+            {
+                MessageBox.Show(listViewCompany.SelectedItems[0].SubItems[1].Text);
+            }
+        }
+    }
+}
